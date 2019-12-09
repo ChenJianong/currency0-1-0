670217662ca0be7c0b19fb4235559aa72dd3c9e9
Glitch (currency0-0-8) <none>
2019-12-09 21:43:14 +0000

    👛🙅 Checkpoint ./public/manifest.json:8229367/2337 ./server.js:8229367/1075 ./public/client.js:8229367/10724 ./public/index.html:8229367/9330 ./public/style.css:8229367/2337

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
+const redirectToHTTPS = require('express-http-to-https').redirectToHTTPS;
 const express = require("express");
 const fetch = require("node-fetch");
 const app = express();
-const redirectToHTTPS = require('express-http-to-https').redirectToHTTPS;
+
+app.use(redirectToHTTPS([/localhost:(\d{4})/], [], 301));
+
+app.use((req, resp, next) => {
+    const now = new Date();
+    const time = `${now.toLocaleDateString()} - ${now.toLocaleTimeString()}`;
+    const path = `"${req.method} ${req.path}"`;
+    const m = `${req.ip} - ${time} - ${path}`;
+    // eslint-disable-nect-line no-console
+    console.log(m);
+    next();
+  });
+
+
 app.use(express.static("public"));
 
  const listener = app.listen(process.env.PORT, function() {
