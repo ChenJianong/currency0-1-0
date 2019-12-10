fa2e8bc382ab30eb43a839c66b28ad66162cb442
Glitch (currency0-0-8) <none>
2019-12-10 22:58:11 +0000

    🌼🕌 Checkpoint ./public/addserviceworker.js:8229367/276 ./public/serviceworker.js:8229367/1651 ./public/offline.html:8229367/658 ./public/index.html:8229367/10029 ./public/service-worker:8229367/1641 ./public/service-worker.js:8229367/1651

---
+++ public/service-worker.js
@@ -0,0 +1,58 @@
+"use strict"; //forces all errors, helps write cleaner code
+
+// CODELAB: Add list of files to cache here.
+const FILES_TO_CACHE = ["/offline.html"];
+
+const CACHE_NAME = "static-cache-v6";
+/* Adds an install event to the page that caches offline resources. */
+self.addEventListener("install", evt => {
+  console.log("[ServiceWorker] Install");
+
+  // CODELAB: Precache static resources here.
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log("[ServiceWorker] Pre-caching offline page");
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+/*once your service worker is ready to control clients and handle
+ *functional events like push and sync, you'll get an activate 3event
+ */
+self.addEventListener("activate", (evt) => {
+  console.log("[Service Worker] Activate");
+
+  // CODELAB: Remove previous cached data from disk.
+  evt.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(
+        keyList.map(key => {
+          if (key !== CACHE_NAME) {
+            console.log("[ServiceWorker] Removing old cache", key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+
+  //start controlling all oaded clients w/o reloading them
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (evt) => {
+  console.log("[ServiceWorker] Fetch", evt.request.url);
+  if (evt.request.mode !== "navigate") {
+    // Not a page navigation, bail.
+    return;
+  }
+  evt.respondWith(
+    fetch(evt.request).catch(() => {
+      return caches.open(CACHE_NAME).then(cache => {
+        return cache.match("offline.html");
+      });
+    })
+  );
+});
